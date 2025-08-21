111d47f37eaff4c1b0667deb30f1876ff3e2b661
Oleksandr Kazan <136016648+San94k@users.noreply.github.com>
2025-08-21 20:08:59 +0200

    Create motivation.py

---
+++ motivation.py
@@ -0,0 +1,8 @@
+import datetime
+
+day = 28
+quote = "Discipline equals freedom. — Jocko Willink"
+
+print(f"🔥 Day {day} of GitHub streak!")
+print(f"Quote of the day: {quote}")
+print(f"Date: {datetime.date.today()}")
